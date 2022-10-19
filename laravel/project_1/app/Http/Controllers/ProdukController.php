b0b0fd752f115f3469b54bb24d4de3f8519c4ef5
Fatan <fatanaqilla90@gmail.com>
2022-10-20 06:35:18 +0700

    Tugas W4D1: Bootstrap, session, sweetalert

--- laravel/project_1/app/Http/Controllers/ProdukController.php
+++ laravel/project_1/app/Http/Controllers/ProdukController.php
@@ -22,6 +22,39 @@ class ProdukController extends Controller
         $produk->nama_produk = $req->nama_produk;
         $produk->stok = $req->stok;
         $produk->save();
+        session(['message' => 'Item added successfully']);
         return redirect('/produk');
+    }
+
+    public function edit($id) {
+        // $detail = Produk::where('id', $id)->get();
+        $detail = Produk::find($id);
+        // return Produk::where('id', $id)->update();
+        session(['message' => 'Item edited successfully']);
+        return view('produk.edit', compact('detail'));
+    }
+
+    public function update($id, Request $req) {
+        $cek = Produk::find($id);
+        if ($cek) {
+            $cek->nama_produk = $req->nama_produk;
+            $cek->stok = $req->stok;
+            $cek->save();
+            return $cek;
+        } else {
+            echo "Data tidak ditemukan";
+        }
+    }
+
+    public function delete($id) {
+        $cek = Produk::find($id);
+
+        if ($cek) {
+            $cek->delete();
+            session(['message' => 'Item deleted successfully']);
+            return redirect("/produk");
+        } else {
+            echo "Data tidak ditemukan";
+        }
     }
 }
--- laravel/project_1/routes/web.php
+++ laravel/project_1/routes/web.php
@@ -27,3 +27,9 @@ Route::get('/produk', [ProdukController::class, 'index']);
 Route::get('/produk/create', [ProdukController::class, 'create']);
 
 Route::post('/produk', [ProdukController::class, 'store']);
+
+Route::get('/produk/edit/{id}', [ProdukController::class, 'edit']);
+
+Route::post('/produk/update/{id}', [ProdukController::class, 'update']);
+
+Route::get('/produk/delete/{id}', [ProdukController::class, 'delete']);
